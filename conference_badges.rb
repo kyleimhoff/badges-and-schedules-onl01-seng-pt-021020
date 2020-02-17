8d112a5389a67c924b829cb9efe3f9cfa00d2e80
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-17 21:23:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,10 @@ def assign_rooms(attendees)
   greeting
 end
 def printer(batch_badge_creator, assign_rooms)
- puts batch_badge_creator
- puts assign_rooms
+ batch_badge_creator.each do |badge|
+   puts badge
+ end
+ assign_rooms.each do |room|
+   puts room
+ end
 end
